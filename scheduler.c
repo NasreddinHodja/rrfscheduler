c423ef29457f311a804ddf8fbf5c1636ab316955
Tomás Bizet <tbizetde@gmail.com>
2021-04-28 17:43:29 -0300

    PID creation

--- scheduler.c
+++ scheduler.c
@@ -8,9 +8,13 @@
 
 enum PRIORITY {low_priority, normal_priority, high_priority};
 enum IO {disk, mag_tape, printer};
+enum PROC_STATUS {running, ready, waiting};
+
+int PROC_COUNT;
 
 typedef struct Process {
   int id;
+  int status;
   int priority;
 } Process;
 
@@ -21,16 +25,22 @@ typedef struct Queue {
   int size;
 } Queue;
 
-Process* p_create(int id, int priority) {
+Process* p_create(int id, int status, int priority) {
   Process* p = (Process*) malloc(sizeof(Process));
+  if(id == -1){
+    id = PROC_COUNT++;
+    PROC_COUNT = PROC_COUNT % MAX_Q;
+  }
   p->id = id;
+  p->status = status;
   p->priority = priority;
   return p;
 }
 
 char* p_to_string(Process* p) {
-  char* a;
-  sprintf(a, "{ PID: %d, priority: %d }", p->id, p->priority);
+  char* a = (char*) malloc(100 * sizeof(char*));
+  sprintf(a, "{ PID: %d, status: %d, priority: %d }", p->id, p->status,
+          p->priority);
   return a;
 }
 
@@ -80,13 +90,20 @@ bool q_push(Queue* q, Process* p) {
 Process* q_pop(Queue* q) {
   if(q->size == 0) return NULL;
   q->size--;
-  Process* p = p_create(q->queue[q->front]->id, q->queue[q->front]->priority);
+  Process* p = p_create(q->queue[q->front]->id, q->queue[q->front]->status,
+                        q->queue[q->front]->priority);
   free(q->queue[q->front]);
   q->queue[q->front++] = NULL;
   return p;
 }
 
+void init() {
+  PROC_COUNT = 0;
+}
+
 int main() {
+  init();
+
   /* Process* processes[4] = {p_create(1), p_create(2), p_create(3)}; */
   /* Queue* q = q_create(processes, sizeof(processes) / sizeof(Process*)); */
   /* Queue* q = q_create(NULL, 0); */
@@ -100,8 +117,12 @@ int main() {
   /* for(int i = 0; i < 10; i++) */
   /*   printf("%d\n", q->queue[idx = q_next_idx(q, idx)]->id); */
 
-  Process* p = p_create(1, low_priority);
-  printf("%s\n", p_to_string(p));
+  Process* p1 = p_create(-1, running, low_priority);
+  Process* p2 = p_create(-1, running, low_priority);
+  Process* p3 = p_create(-1, running, low_priority);
+  printf("%s\n", p_to_string(p1));
+  printf("%s\n", p_to_string(p2));
+  printf("%s\n", p_to_string(p3));
 
   return 0;
 }
